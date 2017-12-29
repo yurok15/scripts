7bad7dacd675e5ef87d6ee1c84268e728604e52b
Yuriy Zhigulskiy <yzhigulskiy@intermedia.net>
2017-12-29 17:11:53 +0300

    NEw version with balancing

--- obzvon.py
+++ obzvon.py
@@ -1,45 +1,100 @@
+#!/usr/bin/python2.7
+###########################################################
+#
+# This python script is used to generate asterisk call files
+#
+# Written by : Yuriy Zhigulskiy
+# email: yurok15@gmail.com
+# github: yurok15@gmail.com
+# Created date: Dec 26, 2017
+# Last modified: Dec 29, 2017
+# Tested with : Python 2.7
+# Script Revision: 0.3
+#
+##########################################################
+
 import logging
 import os
 import time
 import datetime
+import subprocess
 
-logging.basicConfig(filename="obzvon.log", level=logging.INFO)
+start_time = 0
+#end_time = 20
+router_a_ip = '10.78.99.196'
+router_b_ip = '10.78.99.195'
+router_a = 0
+ip = ''
 
-def create_call_file(number):
-    new_file = open('/var/tmp/numbers/%s' %number, 'w+')
-    new_file.write('''Channel: SIP/10.78.99.196/%s
-MaxRetries: 1
-RetryTime: 30
-WaitTime: 30
+logging.basicConfig(format = u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', filename="obzvon.log", level=logging.INFO)
+
+def create_call_file(number, ip):
+    new_file = open('/var/spool/asterisk/outgoing/%s' %number, 'w+')
+    new_file.write('''Channel: SIP/%s/%s
+MaxRetries: 0
+Callerid: %s
+RetryTime: 20
+WaitTime: 20
 Context: pa-system
 Extension: 10
 Priority: 1
-''' % (number))
+''' % (ip, number, number))
+    os.chown('/var/spool/asterisk/outgoing/%s' %number, 995, 1000)
+    logging.info( u'Call file for number %s, got to router (%s) was successfully created' % (number, ip))
 
 def check_file_numbers():
-    return(len(os.listdir('/var/tmp/numbers')))
+    return(len(os.listdir('/var/spool/asterisk/outgoing/')))
 
-def main():
-    with open('new_number2.txt') as file:
+def check_call_numbers():
+    call_numbers = subprocess.Popen(["asterisk -rx 'core show calls' | grep active | cut -d' ' -f1"], stdout=subprocess.PIPE, shell=True)
+    (out, err) = call_numbers.communicate()
+    return(int(out))
+
+def get_router(router_a):
+    if router_a < 2:
+        router_a += 1
+        #print("a")
+        return(router_a, router_a_ip)
+    else:
+        router_a = 0
+        #print("b")
+        return(router_a, router_b_ip)
+
+def file_create_logic(phone_number, router_a, ip):
+    if datetime.date.weekday(datetime.date.today()) < 5:
+        start_time = 11
+    else:
+        start_time = 14
+    while True:
+        now = datetime.datetime.now()
+        if now.hour < start_time or now.hour > 19:
+            logging.warning( u'Bad time for customer call')
+            time.sleep(10)
+            continue
+        else:
+            if check_file_numbers() > 11: #or check_call_numbers() > 10:
+                time.sleep(2)
+                continue
+            else:
+                ###
+                ### function to get next hop VOIP router
+                ###
+                router_a, ip = get_router(router_a)
+                create_call_file(phone_number, ip)
+                break
+    return(router_a)
+
+
+def main(router_a):
+    with open('test_numbers.txt') as file:
         read_data = file.read()
     data_list = read_data.split()
     for phone_number in data_list:
         if int(len(phone_number)) < 11:
-            print('Bad number %s' % (phone_number))
+            logging.warning( u'Bad number %s' % (phone_number))
         else:
-            #check current time
-            now = datetime.datetime.now()
-            if now.hour > 11 and now.hour < 18:
-                # check file numbers in directory
-                if check_file_numbers() < 10:
-                    #create call file
-                    create_call_file(phone_number)
-                else:
-                    time.sleep(2)
-            else:
-                print("Not running time")
-                time.sleep(60)
+            router_a = file_create_logic(phone_number, router_a, ip)
 
 
 if __name__ == "__main__":
-    main()
+    main(router_a)
